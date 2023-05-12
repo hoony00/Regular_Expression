10af9b76832fbafb931d59e478f17332444b3830
Sanghoon Lee <86902854+hoony00@users.noreply.github.com>
2023-05-12 17:04:26 +0900

    Update findEmail.py

--- findEmail.py
+++ findEmail.py
@@ -14,8 +14,8 @@ result2 = re.findall(pattern2, text) #.com 패턴들을 리스트로 가져옴
 
 if result:
     print("이메일 패턴 찾기 성공")
-    print('찾은 이메일 결과:',result) #['sanghoon@testmail.com', 'sanghoon@gmail.com']
-    print('찾은 패턴 수:',len(result))   # 찾은 수: 2
+    print('찾은 이메일 결과:',result)          # ['sanghoon@testmail.com', 'sanghoon@gmail.com']
+    print('찾은 패턴 수:',len(result))         # 찾은 수: 2
 else:
     print('이메일 패턴이 없습니다.')
 
@@ -23,7 +23,7 @@ print("-----------------------------------")
 
 if result2:
     print(".com 패턴 찾기 성공")
-    print('찾은 .com 패턴 결과 :', result2) #.com 찾기 결과 : ['.com', '.com']
+    print('찾은 .com 패턴 결과 :', result2)    #.com 찾기 결과 : ['.com', '.com']
     print('찾은 패턴 수:',len(result2))        # 찾은 수: 3
 else:
     print('찾는 .com 패턴이 없습니다.')
